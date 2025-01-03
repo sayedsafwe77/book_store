843fbb4692000145547a5cd6aab45c71abdf86ce
ssafwet <ssafwet@beyond-creation.net>
2025-01-03 23:09:29 +0200

    create migration files for all tables , add adminlte and make admin authentication

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/dashboard', function () {
+    return view('dashboard.index');
+})->middleware('auth:admin');
+
+
+Route::get('/home',[HomeController::class,'index']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
