ef1c486cf3ed5dde181aa0f733131accf270191e
Mahmoud AbouElawad <35901393+abouelawad@users.noreply.github.com>
2025-01-20 20:37:49 +0200

    add publisher create image feature

--- app/Models/Publisher.php
+++ app/Models/Publisher.php
@@ -2,13 +2,25 @@
 
 namespace App\Models;
 
+use Spatie\Image\Enums\Fit;
 use EloquentFilter\Filterable;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\MediaLibrary\HasMedia;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-class Publisher extends Model
+class Publisher extends Model implements HasMedia
 {
     /** @use HasFactory<\Database\Factories\PublisherFactory> */
-    use HasFactory,Filterable;
+    use HasFactory,Filterable, InteractsWithMedia;
     protected $fillable = ['name'];
+
+    public function registerMediaConversions(Media $media = null): void
+    {
+        $this
+            ->addMediaConversion('preview')
+            ->fit(Fit::Contain, 300, 300)
+            ->nonQueued();
+    }
 }
